307be9832dcad324226d236ea897e0d2fe8f130c
gosha <>
2021-07-25 17:46:15 -0700

    add grid constants and draw blocks to screen

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@
 
 use {
     sfml::{
-        graphics::{Color, RectangleShape, RenderTarget, RenderWindow},
+        graphics::{Color, RectangleShape, RenderTarget, RenderWindow, Shape, Transformable},
+        system::Vector2,
         window::{Event, Key},
     },
     std::collections::LinkedList,
@@ -17,8 +18,12 @@ struct TetrisBlock {
 }
 
 fn main() {
+    let block_size = Vector2::<u32>::new(30, 30);
+    let grid_size = Vector2::<u32>::new(10, 22);
+
+    let window_size = block_size * grid_size;
     let mut window = RenderWindow::new(
-        (675, 675),
+        (window_size.x, window_size.y),
         "Tetris",
         sfml::window::Style::TITLEBAR,
         &Default::default(),
@@ -41,6 +46,26 @@ fn main() {
         }
 
         window.clear(Color::rgb(220, 220, 220));
+        for (y, row) in list_of_rows.iter().enumerate() {
+            for (x, block) in row.iter().enumerate() {
+                let color = match block {
+                    Some(block) => block.color,
+                    None => Color::WHITE,
+                };
+
+                let mut rect = RectangleShape::new();
+                rect.set_outline_color(Color::BLACK);
+                rect.set_outline_thickness(1.0);
+                rect.set_size((block_size.x as f32, block_size.y as f32));
+                rect.set_fill_color(color);
+                rect.set_position((
+                    (x as u32 * block_size.x) as f32,
+                    (y as u32 * block_size.y) as f32,
+                ));
+
+                window.draw(&rect);
+            }
+        }
         window.display();
     }
 }
