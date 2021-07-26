b9e13e9f670200dbd2ee08fd0d0a9f1a3bb585f6
gosha <>
2021-07-25 17:18:40 -0700

    added row list and tetris block

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,19 @@
-use sfml::{
-    graphics::{Color, RenderTarget, RenderWindow},
-    window::{Event, Key},
+#![feature(linked_list_remove)]
+
+use {
+    sfml::{
+        graphics::{Color, RectangleShape, RenderTarget, RenderWindow},
+        window::{Event, Key},
+    },
+    std::collections::LinkedList,
 };
 
 struct TetrisShape<'a> {
     rect: RectangleShape<'a>,
+}
+
+struct TetrisBlock {
+    color: Color,
 }
 
 fn main() {
@@ -14,6 +23,14 @@ fn main() {
         sfml::window::Style::TITLEBAR,
         &Default::default(),
     );
+
+    let mut list_of_rows = LinkedList::<Vec<Option<TetrisBlock>>>::new();
+
+    for _ in 0..22 {
+        let mut row = Vec::new();
+        row.resize_with(10, || None);
+        list_of_rows.push_back(row);
+    }
 
     while window.is_open() {
         while let Some(event) = window.poll_event() {
