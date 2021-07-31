ac8f97f8bdd2f87801dc85603fb6c9851e904542
gosha <>
2021-07-31 12:56:03 -0700

    made a clear line trait and made a fn for checking lines to clear.

--- src/main.rs
+++ src/main.rs
@@ -10,22 +10,38 @@ struct TetrisShape<'a> {
     rect: RectangleShape<'a>,
 }
 
+#[derive(Debug)]
 struct TetrisBlock {
     color: Option<Color>,
+}
+
+trait Move_Line_Down {
+    fn move_line_down(&mut self, line_index: usize);
 }
 
 trait ClearLine {
     fn clear_line(&mut self, _: usize);
 }
 
-impl<T> ClearLine for Vec<Vec<T>> {
+impl ClearLine for Vec<Vec<TetrisBlock>> {
     fn clear_line(&mut self, line_index: usize) {
-        // for index in line_index..self.len() {
-        //     println!("{}", index)
-        // }
-        // for (pos, row) in self.iter_mut().enumerate() {
-        //     self[pos] = self[pos + 1];
-        // }
+        if self.is_empty() {
+            return;
+        }
+
+        for pos in (1..=line_index).rev() {
+            self.swap(pos, pos - 1)
+        }
+
+        for block in self[0].iter_mut() {
+            block.color = None;
+        }
+    }
+}
+
+impl Move_Line_Down for Vec<Vec<TetrisBlock>> {
+    fn move_line_down(&mut self, line_index: usize) {
+        self.swap(line_index, line_index + 1)
     }
 }
 
@@ -43,38 +59,67 @@ fn main() {
 
     let mut vector_of_rows: Vec<Vec<TetrisBlock>> = Vec::new();
 
+    vector_of_rows.resize_with(22, || {
+        let mut row = Vec::new();
+        row.resize_with(10, || TetrisBlock { color: None });
+        row
+    });
+
+    vector_of_rows[3][0].color = Some(Color::BLUE);
+    vector_of_rows[4][0].color = Some(Color::BLACK);
+    vector_of_rows[5][0].color = Some(Color::RED);
+
+    for block in vector_of_rows[14].iter_mut() {
+        block.color = Some(Color::BLUE);
+    }
+    for block in vector_of_rows[2].iter_mut() {
+        block.color = Some(Color::BLUE);
+    }
+    for block in vector_of_rows[20].iter_mut() {
+        block.color = Some(Color::BLUE);
+    }
+
     while window.is_open() {
         while let Some(event) = window.poll_event() {
             match event {
                 Event::Closed | Event::KeyPressed { code: Key::Q, .. } => window.close(),
+                Event::KeyPressed {
+                    code: Key::SPACE, ..
+                } => vector_of_rows.move_line_down(20),
                 _ => (),
             }
         }
 
-        let y = for (pos, value) in vector_of_rows.iter().enumerate() {
-            let y = pos as i32;
-        };
-
-        let x = for row in vector_of_rows.iter() {
-            for (pos, value) in row.iter().enumerate() {
-                let x = pos;
-            }
-        };
-
-        let color = Color::rgb(220, 120, 125);
-
-        let mut rect = RectangleShape::new();
-        rect.set_outline_color(Color::BLACK);
-        rect.set_outline_thickness(1.0);
-        rect.set_size((block_size.x as f32, block_size.y as f32));
-        rect.set_fill_color(color);
-        // rect.set_position((
-        //     (x as u32 * block_size.x) as f32,
-        //     (y as u32 * block_size.y) as f32,
-        // ));
+        while let Some(complete_row_index) = get_completed_row_index(&mut vector_of_rows) {
+            vector_of_rows.clear_line(complete_row_index);
+        }
 
         window.clear(Color::rgb(220, 220, 220));
-        window.draw(&rect);
+        for (y, row) in vector_of_rows.iter().enumerate() {
+            for (x, block) in row.iter().enumerate() {
+                let color = match block.color {
+                    Some(color) => color,
+                    None => Color::WHITE,
+                };
+
+                let mut rect = RectangleShape::new();
+                rect.set_outline_color(Color::BLACK);
+                rect.set_outline_thickness(1.0);
+                rect.set_size((block_size.x as f32, block_size.y as f32));
+                rect.set_fill_color(color);
+                rect.set_position((
+                    (x as u32 * block_size.x) as f32,
+                    (y as u32 * block_size.y) as f32,
+                ));
+
+                window.draw(&rect);
+            }
+        }
         window.display();
     }
+}
+
+fn get_completed_row_index(grid: &Vec<Vec<TetrisBlock>>) -> Option<usize> {
+    grid.iter()
+        .position(|row| row.iter().all(|b| b.color.is_some()))
 }
